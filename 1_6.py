3ed589ed4b0801f6d8efcff336f656ea5b4e3ce4
Jhon5213 <microlabx7@gmail.com>
2024-11-08 01:12:01 +0400

    Add files via upload

---
+++ 1_6.py
@@ -0,0 +1,5 @@
+completed_task_count = 12
+number_of_hours_spent = 1.5
+course_title ='Python'
+time_one_task = 1.5/12
+print("Course"':',course_title,',''total tasks'':',completed_task_count,'hours spent'':',number_of_hours_spent,',''average execution time'':',time_one_task,)
